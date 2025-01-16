c89ea0941a0df19999890f05079aedfda5df440b
evilEnkidu <maganae@uabc.edu.mx>
2025-01-16 15:23:06 -0800

    Adding CRUDs functionality

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GalleryListView
+from .views import GalleryListView, GalleryDetailView
 
 urlpatterns = [
     path('gallery', GalleryListView.as_view(), name='our_work'),
+    path('gallery/<int:pk>/', GalleryDetailView.as_view(), name='gallery_detail'),
 ] 
--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
 from .models import WorkGallery
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 class GalleryListView(ListView):
       model = WorkGallery
       template_name = 'gallery/our_work.html'
       context_object_name = 'galleries'
       ordering = ['-created_at']
-      
+
+class GalleryDetailView(DetailView):
+      model = WorkGallery
+      template_name = 'gallery/detail.html'
+      context_object_name = 'gallery'
+
+      def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['images'] = self.object.images.all()
+            return context 
+
+class GalleryDeleteView(DeleteView):
+      template_name = 'gallery/delete.html'
+      model = WorkGallery
+      success_url = reverse_lazy('our_work')
+
+class GalleryCreateView(LoginRequiredMixin, CreateView):
+      model = WorkGallery
+      template_name = 'gallery/create.html'
+      fields = ['title', 'location', 'description']
+      success_url = reverse_lazy('our_work')
+
